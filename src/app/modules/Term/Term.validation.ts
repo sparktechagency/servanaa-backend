7dd085262a2f82bf16f70329d1f1fc34d1d3795e
Ahmad <ahmadmusa9805@gmail.com>
2025-06-28 09:31:53 +0600

    add some term code

--- src/app/modules/Term/Term.validation.ts
+++ src/app/modules/Term/Term.validation.ts
@@ -2,17 +2,13 @@ import { z } from 'zod';
 
 export const createTermValidationSchema = z.object({
   body: z.object({
-    term: z.object({
       description: z.string().optional(),
-    }),
   }),
 });
 
 export const updateTermValidationSchema = z.object({
   body: z.object({
-    term: z.object({
       description: z.string().optional(),
       isDeleted: z.boolean().optional(),
-    }),
   }),
 });
