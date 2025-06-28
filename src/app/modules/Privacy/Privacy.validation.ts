1bd5e3e652b55610416ba7e2dfba0d40c7e6f540
Ahmad <ahmadmusa9805@gmail.com>
2025-06-28 09:30:45 +0600

    add some privacy correction

--- src/app/modules/Privacy/Privacy.validation.ts
+++ src/app/modules/Privacy/Privacy.validation.ts
@@ -2,17 +2,13 @@ import { z } from 'zod';
 
 export const createPrivacyValidationSchema = z.object({
   body: z.object({
-    privacy: z.object({
       description: z.string().optional(),
-    }),
   }),
 });
 
 export const updatePrivacyValidationSchema = z.object({
   body: z.object({
-    privacy: z.object({
       description: z.string().optional(),
       isDeleted: z.boolean().optional(),
-    }),
   }),
 });
